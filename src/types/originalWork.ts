0a720994b32abcce952f10eb547ddf361e668254
brandnewjinah <brandnewjinah@gmail.com>
2025-02-21 07:00:42 +0900

    fix type error

--- src/types/originalWork.ts
+++ src/types/originalWork.ts
@@ -3,7 +3,15 @@ import { CreditType } from "./credit";
 export interface OriginalWorkType {
     title: string;
     original_title: string;
+    authorId: string;
+    type: string;
+}
+
+export interface OriginalWorkResponseType {
+    _id: string;
+    title: string;
+    original_title: string;
     author: CreditType;
     type: string;
-    hasOriginalWork?: boolean;
+    hasOriginalWork: boolean;
 }
